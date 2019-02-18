fb2f3a1d9471f79d8ce4f3851c42e60798910542
DELL <DELL@windows10.microdone.cn>
2019-02-18 18:59:25 +0800

    r

---
+++ mmp/src/from/LoginFrom.java
@@ -0,0 +1,23 @@
+package from;
+
+import core.ActionForm;
+
+public class LoginFrom extends ActionForm{
+	private String tel;
+	private String upass;
+	
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getUpass() {
+		return upass;
+	}
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}
+
+
+}
